87b134a58780d05265e54449858cb3c5741e145b
Phong Nguyen Dinh <phongnd1203@proton.me>
2024-10-01 12:21:28 +0700

    chore: remove testcase folder

--- FU.OJ.Server/DTOs/Testcase/Request/CreateTestcaseRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace FU.OJ.Server.DTOs.Testcase.Request{    public class CreateTestcaseRequest
-    {
-        [Required]
-        public string ProblemCode { get; set; } = null!;
-        public IFormFile TestcaseFile { get; set; } = null!; //zip only
-    }
-}
